33dc9a0e8c12c197fc2c7a58ed37c8ac25289ef3
Pickelated <123037357+Pickelated@users.noreply.github.com>
2025-07-06 15:23:47 -0700

    yea

--- knight/pickelation.js
+++ knight/pickelation.js
@@ -4,12 +4,12 @@ document.addEventListener('keydown', (event) => {
   console.log(`Key code: ${event.code}`);
 
   
-  if (event.key === 'ArrowDown' || event.key === 's' && z == 1) {
-    document.getElementById("soul").style.top = "357px";
+  if (((event.key === 'ArrowDown' || event.key === 's') && z == 1) || ((event.key === 'ArrowUp' || event.key === 'w') && z == 1)) {
+    document.getElementById("soul").style.top = "50px";
     z = 2;
   }
-  else if (event.key === 'ArrowUp' || event.key === 'w' && z == 2) {
-    document.getElementById("soul").style.top = "312px";
+  else if (((event.key === 'ArrowUp' || event.key === 'w') && z == 2) || ((event.key === 'ArrowDown' || event.key === 's') && z == 2)) {
+    document.getElementById("soul").style.top = "5px";
     z = 1;
   }
 });
