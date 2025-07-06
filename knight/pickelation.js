248e6fd9aeb5fda306c57465f1dc918a6078cece
Pickelated <123037357+Pickelated@users.noreply.github.com>
2025-07-05 23:06:13 -0700

    Update pickelation.js

--- knight/pickelation.js
+++ knight/pickelation.js
@@ -1,1 +1,15 @@
-yeah
+let z = 1;
+document.addEventListener('keydown', (event) => {
+  console.log(`Key pressed: ${event.key}`);
+  console.log(`Key code: ${event.code}`);
+
+  
+  if (event.key === 'z' && z == 1) {
+    document.getElementById("soul").style.top = "355px";
+    z = 2;
+  }
+  if (event.key === 'z' && z == 2) {
+    document.getElementById("soul").style.top = "315px";
+    z = 1;
+  }
+});
