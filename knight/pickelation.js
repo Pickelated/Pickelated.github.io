679a0753ffce15c74aea800961468a33105697ad
Pickelated <123037357+Pickelated@users.noreply.github.com>
2025-07-06 20:23:21 -0700

    hello

--- knight/pickelation.js
+++ knight/pickelation.js
@@ -2,6 +2,7 @@ let z = 1; //main menu z
 let x = 1; //loadout menu x (top)
 let soulx = 1; //default soul x pos
 let souly = 1; //default soul y pos
+let menuing = false;
 let mainmenu = true;
 let loadoutmenu = false;
 
@@ -13,8 +14,8 @@ document.addEventListener('keydown', (event) => {
   console.log(`Key pressed: ${event.key}`);
   console.log(`Key code: ${event.code}`);
 
-  mainMenu();
   loadoutMenu();
+  mainMenu();
 
 });
 
@@ -37,12 +38,16 @@ function mainMenu() {
     document.getElementById("title").style.display = "none";
     document.getElementById("loadout").style.display = "block";
     document.getElementById("soul").style.display = "inline";
+    document.getElementById("krisThumb").style.filter = "grayscale(0)";
+    document.getElementById("susieThumb").style.filter = "grayscale(0)";
+    document.getElementById("ralseiThumb").style.filter = "grayscale(0)";
     mainmenu = false;
     loadoutmenu = true;
+    x = 1;
     audSelect.play();
     audSelect.currentTime = 0;
-    souly = 52; //px
-    soulx = 230; //px
+    souly = 53; //px
+    soulx = 92; //px
   }
   if ((event.key === "z" || event.key === "j") && z == 1) {
     alert("WHAT");
@@ -56,26 +61,64 @@ function mainMenu() {
 
 function loadoutMenu() {
   if (loadoutmenu == true) {
-    if (event.key === 'x' || event.key === 'k') {
+    if ((event.key === 'x' || event.key === 'k') && menuing == false) {
       loadoutmenu = false;
       mainmenu = true;
       document.getElementById("title").style.display = "block";
       document.getElementById("loadout").style.display = "none";
+      document.getElementById("ssoul").style.top = "53px";
+      document.getElementById("ssoul").style.left = "92px";
       audSelect.play();
       audSelect.currentTime = 0;
     }
+    else if ((event.key === 'x' || event.key === 'k') && menuing == true) {
+      menuing = false;
+      document.getElementById("krisThumb").style.filter = "grayscale(0)";
+      document.getElementById("susieThumb").style.filter = "grayscale(0)";
+      document.getElementById("ralseiThumb").style.filter = "grayscale(0)";
+    }
 
-    if ((event.key === 'ArrowRight' || event.key === 'd') && x < 3) {
+    if ((event.key === 'ArrowRight' || event.key === 'd') && x < 3 && menuing == false) {
       soulx += 380;
       x++;
+      audMove.play();
+      audMove.currentTime = 0;
       document.getElementById("ssoul").style.left = soulx + "px";              // TOP menu header thingy kris susie ralsei
     }
-    if ((event.key === 'ArrowLeft' || event.key === 'a') && x > 1) {
+    if ((event.key === 'ArrowLeft' || event.key === 'a') && x > 1 && menuing == false) {
       soulx -= 380;
       x--;
+      audMove.play();
+      audMove.currentTime = 0;
       document.getElementById("ssoul").style.left = soulx + "px";
     }
+    if ((event.key === "z" || event.key === "j") && x == 1) { //krs
+      
+      menuing = true;
+      document.getElementById("susieThumb").style.filter = "grayscale(1)";
+      document.getElementById("ralseiThumb").style.filter = "grayscale(1)";
 
+      audSelect.play();
+      audSelect.currentTime = 0;
+    }
+    if ((event.key === "z" || event.key === "j") && x == 2) { //sus
+      
+      menuing = true;
+      document.getElementById("krisThumb").style.filter = "grayscale(1)";
+      document.getElementById("ralseiThumb").style.filter = "grayscale(1)";
+
+      audSelect.play();
+      audSelect.currentTime = 0;
+    }
+    if ((event.key === "z" || event.key === "j") && x == 3) { //ral
+      
+      menuing = true;
+      document.getElementById("krisThumb").style.filter = "grayscale(1)";
+      document.getElementById("susieThumb").style.filter = "grayscale(1)";
+
+      audSelect.play();
+      audSelect.currentTime = 0;
+    }
 
   }
 }
