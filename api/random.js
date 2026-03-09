0b05b3e253dc699e07124f87666f196923f6ecb6
Pickelated <cordero.gutierrezfamily@gmail.com>
2026-03-08 18:30:41 -0700

    dude

--- api/random.js
+++ api/random.js
@@ -6,6 +6,11 @@ export default function handler(req, res) {
   ];
 
   const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
-  res.writeHead(302, { Location: randomGif });
+
+  // 302 redirect to static GIF
+  res.writeHead(302, { 
+    'Location': randomGif,
+    'Content-Type': 'image/gif'   // ensure correct header
+  });
   res.end();
 }
